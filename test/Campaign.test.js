02e07ed1a4e85d10799a241b887683cfff81269d
Alex Lee <alex@uniteus.com>
2018-03-15 12:18:42 -0400

    Fix contract public access

---
+++ test/Campaign.test.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)) // interface ABI
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000',
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call(); // take first element of the array
+
+  // No need to execute .deploy or .send since its already deployed via factory
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe('Campaigns', () => {
+  it('deploys a factory and campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('allows people to contribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    });
+
+    console.log(await campaign.methods.approversCount().call());
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    //
+    // console.log(isContributor);
+    // assert(isContributor);
+  });
+});
